968aa8659b7e304f367c6714cd636d875002badb
Gabriel Ramos <gabrieltech209@gmail.com>
2026-08-21 16:50:52 -0300

    feat: Adição do componente Hero, porém ainda sem estilização

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,7 +1,34 @@
+import {FaGithub, FaLinkedin} from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import styles from "./Hero.module.css";
 function Hero() {
     return(
-        <section>
-            
+        <section id={styles.hero}>
+            <div className={styles.heroContent}>
+                <span>DESENVOLVEDOR FULLSTACK</span>
+                <h1 className={styles.heroTitle}>Gabriel Ramos</h1>
+                <h2 className={styles.heroSubtitle}>Construo soluções robustas, escaláveis e eficientes</h2>
+                <p className={styles.heroText}>Desenvolvedor de software com foco em </p>
+                <div className={styles.containerHeroButtons}>
+                    <a>Ver meus projetos</a>
+                    <a>Baixar currículo</a>
+                </div>
+                <div className={styles.containerHeroSocial}>
+                    <a>
+                        <FaGithub />
+                        
+                    </a>
+                    <a>
+                        <FaLinkedin />
+                    </a>
+                    <a>
+                        <HiOutlineMail />
+                    </a>
+                </div>
+            </div>
+            <div>
+                <img />
+            </div>
         </section>
     )
 }
