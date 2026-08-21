9ac97c41de7fd948a22f4810971420462ba58ae0
Gabriel Ramos <gabrieltech209@gmail.com>
2026-08-21 19:15:12 -0300

    refactor: Alterada os nomes das variáveis de tamanhos para nomes mais explícitos

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,33 +1,43 @@
 import {FaGithub, FaLinkedin} from "react-icons/fa";
-import { HiOutlineMail } from "react-icons/hi";
+import {HiOutlineMail} from "react-icons/hi";
 import styles from "./Hero.module.css";
+
 function Hero() {
-    return(
+    return (
         <section id={styles.hero}>
             <div className={styles.heroContent}>
-                <span>DESENVOLVEDOR FULLSTACK</span>
-                <h1 className={styles.heroTitle}>Gabriel Ramos</h1>
-                <h2 className={styles.heroSubtitle}>Construo soluções robustas, escaláveis e eficientes</h2>
-                <p className={styles.heroText}>Desenvolvedor de software com foco em </p>
+                <div>
+                    <span>DESENVOLVEDOR FULLSTACK</span>
+                    <h1 className={styles.heroTitle}>Gabriel Ramos</h1>
+                    <h2 className={styles.heroSubtitle}>Construo soluções robustas, escaláveis e eficientes</h2>
+                    <p className={styles.heroText}>Desenvolvedor Fullstack criando aplicações modernas do backend à interface. Transformo ideias em produtos funcionais, bem estruturados e pensados para oferecer uma boa experiência do início ao fim. </p>
+
+                </div>
                 <div className={styles.containerHeroButtons}>
                     <a>Ver meus projetos</a>
                     <a>Baixar currículo</a>
                 </div>
                 <div className={styles.containerHeroSocial}>
                     <a>
-                        <FaGithub />
-                        
+                        <FaGithub/>
+
                     </a>
                     <a>
-                        <FaLinkedin />
+                        <FaLinkedin/>
                     </a>
                     <a>
-                        <HiOutlineMail />
+                        <HiOutlineMail/>
                     </a>
                 </div>
             </div>
-            <div>
-                <img />
+            <div className={styles.heroImage}>
+                <div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <span>gabrielramos.com</span>
+                </div>
+                <img/>
             </div>
         </section>
     )
